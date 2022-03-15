0cd663df3305be7937449bffc5fff9d928e127e7
Prozoreanu Eduard <prozoreanueduard12@yahoo.com>
2022-03-15 18:19:25 +0200

    From class to java files aded

---
+++ FromClassToJava/Partener.java
@@ -0,0 +1,15 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+public class Partener
+{
+    public String nume;
+    public String codFirma;
+    
+    public void creareContPartener() {
+    }
+    
+    public void gestioneazaProduse() {
+    }
+}
